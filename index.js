9b35dfdd33318e77bfd8fa8f6bec99ae594c4e5c
git config --global user.name <mahek1542000@gmail.com>
2022-09-21 12:46:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const yargs = require('yargs')
+    // Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Adds two number',
+    builder: {
+        firstNumber: {
+            describe: 'First Number',
+            demandOption: true, // Required
+            type: 'number'
+        },
+        secondNumber: {
+            describe: 'Second Number',
+            demandOption: true,
+            type: 'number'
+        }
+    },
+
+    // Function for your command
+    handler(argv) {
+        console.log("Result:",
+            (argv.firstNumber + argv.secondNumber))
+    }
+})
+
+yargs.parse() // To set above changes
